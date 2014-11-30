95a3288ad82540f731190b83d1191c6eea3842ec
MichaelMcAfee <michaelthomasmcafee@gmail.com>
2014-11-29 21:04:21 -0600

    Fixed sales and winning/losing thing

--- ConversationParser.java
+++ ConversationParser.java
@@ -1,126 +1,185 @@
 import java.util.Scanner;
 public class ConversationParser
 {
-    Game game = new Game();
-    public void conversationOnePrompt()
+    public void customerOpen()
+    {
+        System.out.print('\u000C');
+        System.out.println("    ___________  ");
+        System.out.println("   |           | ");
+        System.out.println("   |  |     |  | ");
+        System.out.println("   |    ___    | ");
+        System.out.println("   |   |___|   | ");
+        System.out.println("    - - - - - -  ");
+        System.out.println("        /|\\      ");
+        System.out.println("       / | \\     ");
+        System.out.println("      /  |  \\    ");
+        System.out.println("     /   |   \\   ");
+        System.out.println("         |       ");
+        System.out.println("         |       ");
+    }
+    
+    public void customerClosed()
+    {
+        System.out.print('\u000C');
+        System.out.println("    ___________  ");
+        System.out.println("   |           | ");
+        System.out.println("   |  |     |  | ");
+        System.out.println("   |           | ");
+        System.out.println("   |   -----   | ");
+        System.out.println("    - - - - - -  ");
+        System.out.println("        /|\\      ");
+        System.out.println("       / | \\     ");
+        System.out.println("      /  |  \\    ");
+        System.out.println("     /   |   \\   ");
+        System.out.println("         |       ");
+        System.out.println("         |       ");
+    }
+    
+    public void customer()
+    {
+        Game game = new Game();
+        System.out.print('\u000C');
+        customerClosed();
+        game.sleepShort();
+        customerOpen();
+        game.sleepShort();
+        customerClosed();
+        game.sleepShort();
+        customerOpen();
+        game.sleepShort();
+        customerClosed();
+        game.sleepShort();
+        customerOpen();
+        game.sleepShort();
+        customerClosed();
+        game.sleepShort();
+        customerOpen();
+        game.sleepShort();
+        customerClosed();
+    }
+    
+    public void conversationOnePrompt(String item)
     {
         Scanner s = new Scanner(System.in);
-        game.type("Customer: What do you want?");
-        game.sleepShort();
+        Game.type("Customer: What do you want?");
+        Game.sleepShort();
         System.out.println("");
-        game.type("You can say:");
+        Game.type("You can say:");
         System.out.println("");
-        game.type("A) Hi, I have an incredible opportunity for you!");
+        Game.type("A) Hi, I have an incredible opportunity for you!");
         System.out.println("");
-        game.type("B) Hello, would you like to buy " + "[x]" + "?");
+        Game.type("B) Hello, would you like to buy " + item + "?");
         System.out.println("");
-        game.type("C) I have something that will make your day!");
+        Game.type("C) I have something that will make your day!");
         System.out.println("");
-        game.type("You can type a, b, or c");
+        Game.type("You can type a, b, or c");
         System.out.println("");
         System.out.print("> ");
         String input = s.nextLine(); // Get next line of user input
         if (input.toLowerCase().equals("a"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Go away, I don't want any part of your scam!");
+            Game.type("Customer: Go away, I don't want any part of your scam!");
             System.out.println("");
         }
         else if (input.toLowerCase().equals("b"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Why yes, I would love some " + "[x]" + "! I will buy 5!");
+            Game.type("Customer: Why yes, I would love some " + item + "! I will buy 5!");
             System.out.println("");
         }
         else if (input.toLowerCase().equals("c"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Oh. Cool.");
+            Game.type("Customer: Oh. Cool.");
             System.out.println("");
         }
     }
     
-    public void conversationTwoPrompt()
+    public int conversationTwoPrompt(String item)
     {
         Scanner s = new Scanner(System.in);
-        game.type("Customer: What do you want?");
-        game.sleepShort();
+        Game.type("Customer: What do you want?");
+        Game.sleepShort();
         System.out.println("");
-        game.type("You can say:");
+        Game.type("You can say:");
         System.out.println("");
-        game.type("A) Hi, I have an incredible opportunity for you!");
+        Game.type("A) Hi, I have an incredible opportunity for you!");
         System.out.println("");
-        game.type("B) Hello, would you like to buy " + "[x]" + "?");
+        Game.type("B) Hello, would you like to buy " + item + "?");
         System.out.println("");
-        game.type("C) I have something that will make your day!");
+        Game.type("C) I have something that will make your day!");
         System.out.println("");
-        game.type("You can type a, b, or c");
+        Game.type("You can type a, b, or c");
         System.out.println("");
         System.out.print("> ");
         String input = s.nextLine(); // Get next line of user input
+        int totalItems = 0;
         if (input.toLowerCase().equals("a"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Go away, I don't want any part of your scam!");
+            Game.type("Customer: Go away, I don't want any part of your scam!");
             System.out.println("");
         }
         else if (input.toLowerCase().equals("b"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Why yes, I would love some " + "[x]" + "! I will buy 5!");
+            Game.type("Customer: Why yes, I would love some " + item + "! I will buy 5!");
+            totalItems = totalItems = 5;
             System.out.println("");
         }
         else if (input.toLowerCase().equals("c"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Oh. Cool.");
+            Game.type("Customer: Oh. Cool.");
             System.out.println("");
         }
+        return totalItems;
     }
     
-    public void conversationThreePrompt()
+    public void conversationThreePrompt(String item)
     {
         Scanner s = new Scanner(System.in);
-        game.type("Customer: What do you want?");
-        game.sleepShort();
+        Game.type("Customer: What do you want?");
+        Game.sleepShort();
         System.out.println("");
-        game.type("You can say:");
+        Game.type("You can say:");
         System.out.println("");
-        game.type("A) Hi, I have an incredible opportunity for you!");
+        Game.type("A) Hi, I have an incredible opportunity for you!");
         System.out.println("");
-        game.type("B) Hello, would you like to buy " + "[x]" + "?");
+        Game.type("B) Hello, would you like to buy " + item + "?");
         System.out.println("");
-        game.type("C) I have something that will make your day!");
+        Game.type("C) I have something that will make your day!");
         System.out.println("");
-        game.type("You can type a, b, or c");
+        Game.type("You can type a, b, or c");
         System.out.println("");
         System.out.print("> ");
         String input = s.nextLine(); // Get next line of user input
         if (input.toLowerCase().equals("a"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Go away, I don't want any part of your scam!");
+            Game.type("Customer: Go away, I don't want any part of your scam!");
             System.out.println("");
         }
         else if (input.toLowerCase().equals("b"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Why yes, I would love some " + "[x]" + "! I will buy 5!");
+            Game.type("Customer: Why yes, I would love some " + item + "! I will buy 5!");
             System.out.println("");
         }
         else if (input.toLowerCase().equals("c"))
         {
-            game.customer();
+            customer();
             System.out.println("");
-            game.type("Customer: Oh. Cool.");
+            Game.type("Customer: Oh. Cool.");
             System.out.println("");
         } 
     }
